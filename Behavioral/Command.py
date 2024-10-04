fb4ed1cd840989f4181114fc1649e75f7419b85b
Khai PHAM (Mr.Kay) <khaipham2204@gmail.com>
2024-10-04 12:41:42 +0700

    Update Command.py

--- Behavioral/Command.py
+++ Behavioral/Command.py
@@ -1,67 +1,67 @@
 from __future__ import annotations
-from dataclasses import dataclass
 from abc import ABC, abstractmethod
+from dataclasses import dataclass, field
 
-class ICommand(ABC):
-    ''' This interface Command '''
+class Command(ABC):
+    """
+    The Command interface declares a method for executing a command.
+    """
     @abstractmethod
-    def execute(self,):
-        pass
+    def execute(self,) -> None:
+        raise NotImplementedError("You should implement this method.")
+    
     
 @dataclass
-class SimpleCommand(ICommand):
-    _payload : str
-    def execute(self) -> None:
-        print(f"SimpleCommand: See, I can do simple things like printing"
-            f"({self._payload})"
-        )
+class NotifyCommand(Command):
     
-@dataclass
-class ComplexCommand(ICommand):
-    _reciever: Receiver
-    _param: str
-    def execute(self) -> None:
-        self._reciever.do_something(self._param)
+    _receiver: object
+    _message: dict
+    _consumer: dict
     
+    async def execute(self) -> any:
+        
+        data = self._receiver.notify(self._message)
+    
+        await self._consumer.send_json({
+            "data": data,
+            "status": 200
+        })
+        
 @dataclass
+class ChatMessageCommand(Command):
+    _receiver: object
+    _message: dict
+    _consumer: dict
+    
+    async def execute(self) -> None:
+        print(f"ChatMessageCommand: {self._message}")
+        await self._receiver.chat_message(self._message)
+    
 class Receiver:
-    def do_something(self, param: str) -> None:
-        print(f"\nReceiver: Working on ({param}.)", end="")
+    def notify(self, msg: dict) -> dict:
+        print(f"Message sent: {msg}")
+        return "Hello from Notification event"
+    
+    def chat_message(self, msg: dict) -> None:
+        print(f"Message sent: {msg}")
 
 @dataclass
 class Invoker:
-    ''' Commands '''
-    _on_light = None
-    _off_light = None
     
-    ''' set of commands'''
-    def set_on_light(self, command: ICommand):
-        self._on_light = command
+    _commands: dict = field(default_factory=dict)
     
-    def set_off_light(self,command: ICommand):
-        self._off_light = command
+    def register(self, event_name, command):
+        self._commands[event_name] = command
     
-    '''Excute command '''
-    def executeCommand(self,):
-        print("Invoker: Does anybody want something done before I begin?")
-        if isinstance(self._on_light, ICommand):
-            self._on_light.execute()
-            
-        print("Invoker: Does anybody want something done after I finish?")
-        if isinstance(self._off_light, ICommand):
-            self._off_light.execute()
+    async def execute(self, event_name):
+        if event_name in self._commands:
+            await self._commands[event_name].execute()
+        else:
+            print(f"Command for event {event_name} not found")
         
                 
 if __name__ == "__main__":
-    
     sender = Invoker()
-    cmd = SimpleCommand("ON")
-    sender.set_on_light(cmd)
-    # sender.executeCommand()
-  
     receiver = Receiver()
-    param = "Hello"
-    cmd = ComplexCommand(receiver, "Send email")
-    sender.set_off_light(cmd)
-    
-    sender.executeCommand()  
+    sender.register('chat', NotifyCommand(self.receiver, event, consumer))
+    await sender.execute('chat')
